bfcd0fa3c9cafe7078742671b1fbf55f7b7ce96e
Jeffery Lin <jeffery.lin@student.tdsb.on.ca>
2025-06-03 14:44:38 -0400

    Fixed movie view genres

--- src/views/MoviesView.jsx
+++ src/views/MoviesView.jsx
@@ -3,25 +3,15 @@ import Header from '../components/Header';
 import Genres from '../components/Genres';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { useStoreContext } from '../context/index.jsx';
 
 function MoviesView() {
-    const genres = [
-        { genre: "Action", id: 28 },
-        { genre: "Animation", id: 16 },
-        { genre: "Crime", id: 80 },
-        { genre: "Sci-Fi", id: 878 },
-        { genre: "Mystery", id: 9648 },
-        { genre: "Adventure", id: 12 },
-        { genre: "Family", id: 10751 },
-        { genre: "History", id: 36 },
-        { genre: "Fantasy", id: 14 },
-        { genre: "Horror", id: 27 }
-    ];
+    const { selectedGenres } = useStoreContext();
 
     return (
         <div className='movies'>
             <Header />
-            <Genres genresList={genres} />
+            <Genres genresList={selectedGenres} />
             <div className='genre-movies'>
                 <Outlet />
             </div>
